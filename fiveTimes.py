fee6799006f7c69750d81b2609f9433afe5cdccf
d3r3k6 <38481105+d3r3k6@users.noreply.github.com>
2018-04-21 20:28:43 -0500

    Fixed counter
    It was broker

--- fiveTimes.py
+++ fiveTimes.py
@@ -1,5 +1,13 @@
 #This is a simple program to practice the range function
-
 print('My name is')
+
 for i in range(5):
+
 	print('Derek Five Times (' + str(i) + ')')
+#This is a way to impliment it with a while lopp
+print("Again my name is")
+i = 0
+while i <= 5:
+	print('Derek 5 more times (' + str(i) + ')')
+	i = i + 1
+	
