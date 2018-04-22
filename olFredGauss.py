b473517f65afa3731a29edf7ea67d15e790708cb
d3r3k6 <38481105+d3r3k6@users.noreply.github.com>
2018-04-21 20:17:28 -0500

    Fred Gauss is Smart
    We just program

---
+++ olFredGauss.py
@@ -0,0 +1,8 @@
+#Supposedly Friedricg Guass was good at counting and summed up the numbers beter 0 and 100 quickly
+#We are going to let Python do the heavy lifting for us
+
+total = 0
+for num in range(101):
+	total = total + num
+print(total)
+	
