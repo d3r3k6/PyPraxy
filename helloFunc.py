5847b990cde61e65c262d4fcf95cc43713ce507f
d3r3k6 <38481105+d3r3k6@users.noreply.github.com>
2018-04-24 08:28:52 -0500

    Add Function Porgrams
    Added two new programs to repo

---
+++ helloFunc.py
@@ -0,0 +1,8 @@
+def hello():
+	print('howdy')
+	print('Howdy!!!')
+	print('Hello, There.')
+hello()
+hello()
+hello()
+	
---
+++ helloFunc2.py
@@ -0,0 +1,7 @@
+#Simple program to learn how to pass arguments to a function
+#The parameter is the variable that an argument is stored in when fnc is called
+def hello(name):
+	print('Hello ' + name)
+hello('Alice')
+hello('Bob')
+	
