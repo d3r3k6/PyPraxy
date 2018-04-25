46c698a8b7b14fc48fbd70de607eed7e2f33f4c3
d3r3k6 <38481105+d3r3k6@users.noreply.github.com>
2018-04-25 15:03:31 -0500

    Collatz Sequence Calculator
    Evaluates collatz sequence for all positive intigers

--- collatz.py
+++ collatz.py
@@ -1,29 +1,16 @@
-#This is a program that explores the Collatz Sequence
-#This is an algorithm that wil evaluate all positive integers down to 1 as far as we know
+#This program evaluates the collatz sequenze for all positive intigers
+
 
 def collatz(number):
-    if number % 2 == 0:
-        print(number // 2)
-        return number // 2
-    elif number % 2 == 1:
-        result = 3 * number + 1
-        print(result)
-        return result
-
-n = input("Enter a positive integer: ")
-while n != 1:
-    n = collatz(int(n))
-		
-
-			
+	if number % 2 == 0:
+		number = number // 2
+		print(number)
+	else:
+		number = 3 * number + 1
+		print(number)
+	return number
+print("Please input a positive intiger greater than 1")
+n = int(input())
+while n!= 1:
+	n = collatz(n)
 	
-	
-	
-	
-
-
-
-
-		
-	
-
