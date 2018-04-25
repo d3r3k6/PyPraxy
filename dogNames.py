d547c2874d195bbf1da394241cf0bad327c514d3
d3r3k6 <38481105+d3r3k6@users.noreply.github.com>
2018-04-25 15:58:51 -0500

    Adds names to a list
    Program to add names to list

---
+++ dogNames.py
@@ -0,0 +1,12 @@
+#Program to practice adding items to a list
+
+dogNames = []
+while True:
+	print('Enter the name of dog ' + str(len(dogNames) + 1) + ' or enter nothing to stop.')
+	name = input()
+	if name == '':
+		break
+	dogNames = dogNames + [name] #list concatenation
+print("The dog's names are:")
+for name in dogNames:
+	print(' ' + name)	
