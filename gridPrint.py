84a88f8e25b45a4a9d8fe2a400af22644f37f496
d3r3k6 <38481105+d3r3k6@users.noreply.github.com>
2018-04-28 20:24:12 -0500

    Prints Grid by Coord
    Prints the grid by coordinates

--- gridPrint.py
+++ gridPrint.py
@@ -9,8 +9,15 @@ grid = [['.', '.', '.', '.', '.', '.'],
         ['O', 'O', 'O', 'O', '.', '.'],
         ['.', 'O', 'O', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.']]
-		
 
+#This is probably the way to do it based on what is presented so far
+
+for a in range(len(grid[0])):
+	for b in range(0, len(grid)):
+		print(grid[b][a], end ='')
+	print()	
+	
+print('.........')
 		
 		
 #This is the elegant answer	using the zip function	
