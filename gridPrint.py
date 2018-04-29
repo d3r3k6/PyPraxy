02758a26ac4d4fd681119e4a9082b8753c7155ea
d3r3k6 <38481105+d3r3k6@users.noreply.github.com>
2018-04-28 19:23:06 -0500

    gridPrint
    Program designed to print a grid

---
+++ gridPrint.py
@@ -0,0 +1,21 @@
+#Design a program to print the grid
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+		
+
+		
+		
+#This is the elegant answer	using the zip function	
+def fish():
+    for line in zip(*grid):
+        print(''.join(line))
+fish()
+		
