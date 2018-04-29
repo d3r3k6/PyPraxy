0d518356ad98b87d2c06d2be08db8cb7fbc17c65
d3r3k6 <38481105+d3r3k6@users.noreply.github.com>
2018-04-28 20:41:11 -0500

    Birthday Database
    Program to store bdays

---
+++ birthdays.py
@@ -0,0 +1,18 @@
+
+birthdays = {'Alice': 'April 1', 'Bob': 'Dec 12', 'Carol': 'Mar 4'}
+
+while True:
+	print('Enter a name: (blank to quite)')
+	name = input()
+	if name == '':
+		break
+
+	if name in birthdays:
+		print(birthdays[name] + ' is the birthday of' + name)
+	else:
+		print('I do not have birthday info for ' + name)
+		print('What is thier birthday?')
+		bday = input()
+		birthdays[name] = bday
+		print('Birthday database updated.')
+			
