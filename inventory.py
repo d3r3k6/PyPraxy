36a7751e37515c39c17e2726617d995d00df0072
d3r3k6 <38481105+d3r3k6@users.noreply.github.com>
2018-05-01 11:35:43 -0500

    Return to This
    Return to this program to finish

--- inventory.py
+++ inventory.py
@@ -4,17 +4,20 @@ stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 #This function print inventory
 def dispalyInv(inventory):
     print("Your Current Inventory")
-    item_total = 0
     for k, v in inventory.items():
         print(str(v) + ' ' + k)
-        item_total += v
-    print(item_total + ' Items in inventory')
 dispalyInv(stuff)
 
-#Congats! You slayed a dragon. Below is his Inventory
+print("Congats! You slayed a dragon. Below is his Inventory")
 
 dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 
+print(dragonLoot)
 #Function to add dragons inventory to yoru Inventory
 
-def addToInv()
+def addToInv():
+    for i in dragonLoot:
+        stuff.setdefault("i", 0)
+        stuff[i] = stuff[i] + 1
+addToInv(dragonLoot)
+print(stuff)
